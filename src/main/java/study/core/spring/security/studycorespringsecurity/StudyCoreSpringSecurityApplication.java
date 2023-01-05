c52fda55a27ba523fda9ded91c8f39e8a3aaec2a
howisitgoing <howisitgoing@kakao.com>
2023-01-06 01:33:20 +0900

    Initial commit

---
+++ src/main/java/study/core/spring/security/studycorespringsecurity/StudyCoreSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package study.core.spring.security.studycorespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCoreSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCoreSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/core/spring/security/studycorespringsecurity/StudyCoreSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package study.core.spring.security.studycorespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCoreSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
